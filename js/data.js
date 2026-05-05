fce5d26e7de29faf0a887098c5285b69b440030c
weyimi barrow <oritseweyinmibarrow@gmail.com>
2026-05-04 21:19:59 -0600

    initial commit: project structure and Ghana temp data

---
+++ js/data.js
@@ -0,0 +1,37 @@
+const countries = [
+    {
+        countryName: "Ghana",
+        countryID: "GH",
+        prevCountryName: "Gold Coast",
+        region: "West Africa",
+        coloniser: "The British Empire",
+        indYear: 1957,
+        indDay: "March 6",
+        firstDemocraticLeader: "Kwame Nkrumah",
+        quoteText:"We face neither East nor West; we face forward.",
+        quoteAuthor: "Kwame Nkrumah",
+        flagColors: ["#EF3B27", "#FFCD00", "#006B3F", "#000000"],
+
+        timelineEvents: [
+            {
+                year: 1880,
+                eventTitle: "The Berlin Conference",
+                eventSummary: "European powers meet to formally divide Africa, placing the Gold Coast (Ghana), under British control.",
+                eventImage: "assets/images/ghana-1957"
+            },
+            {
+                year: 1947,
+                eventTitle: "Formation of the United Gold Coast Convention (UGCC)",
+                eventSummary: "The UGCC is formed to advocate for self-governance and independence from British rule.",
+                eventImage: "assets/images/ghana-1957"
+            },
+            {
+                year: 1957,
+                eventTitle: "Independence Day",
+                eventSummary: "Ghana gains independence from British rule.",
+                eventImage: "assets/images/ghana-1957"
+            },
+            
+        ]
+    }
+]
