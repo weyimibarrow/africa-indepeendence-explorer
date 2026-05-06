89ba7cf2b6d076012b80e544e780e4763bd1f92d
weyimi barrow <oritseweyinmibarrow@gmail.com>
2026-05-06 16:02:21 -0600

    added starfield with CSS and JS, landing screen semi functional

---
+++ js/stars.js
@@ -0,0 +1,10 @@
+const starfield = document.getElementById("starfield");
+for (let i = 0; i < 150; i++){
+    const star = document.createElement("div");
+    star.classList.add("star");
+    star.style.top = Math.random() * 100 + "%";
+    star.style.left= Math.random() * 100 + "%";
+    star.style.width= Math.floor(Math.random() * 2 + 1) + "px";
+    star.style.height = Math.floor(Math.random() * 2 + 1) + "px";
+    starfield.appendChild(star);
+}
