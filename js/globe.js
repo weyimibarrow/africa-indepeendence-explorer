2e97d5e589170466897c420126788c42338f5f0c
weyimi barrow <oritseweyinmibarrow@gmail.com>
2026-05-06 23:32:38 -0600

    added rotation to globe, anticlockwise rn tho. will review laterr

--- js/globe.js
+++ js/globe.js
@@ -6,4 +6,9 @@ const myGlobe = Globe()(globeContainer)
     .width(700)
     .height(700)
     .backgroundColor('rgba(0,0,0,0)')
+    setTimeout(function() {
+        console.log(myGlobe.controls()); // this was to check the console log for the rotation controls, debugging purposes. 
+         myGlobe.controls().autoRotate = true;
+        myGlobe.controls().autoRotateSpeed = 0.8;
+    }, 1000);
 });
